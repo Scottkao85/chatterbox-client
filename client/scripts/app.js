48392cf043c4ccb8a7d5a00f122b5fb46acb6183
scottkao85 <scottkao85@gmail.com>
2015-02-17 16:07:44 -0800

    meet basic requirement

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,7 +20,7 @@ app.fetch = function(url){
     datatype: "json",
     success: function(data){
       compareMessages(data.results);
-      _.each(app.messages, app.addMessage);
+      _.each(app.messages[app.currentRoom], app.addMessage);
     }
   });
 }
@@ -30,43 +30,80 @@ app.clearMessages = function(){
 
 app.rooms = {'4chan' : '4chan'};
 
-app.messages = [{}];
+app.currentRoom = '4chan';
 
-// (app.message[i].text === )
+app.messages = {length: 1,
+                '4chan': [],
+                };
+
 var compareMessages = function(messages){
-  //debugger;
-  for(var i = 0; i < messages.length; i++) {
+  for(var i = 0; i < messages.length; i++){
+    var name = messages[i].roomname;
     var hasDuplicate = false;
-    for(var j = 0; j < app.messages.length; j++){
+    if(app.messages[name] === undefined){
+      app.messages[name] = [];
+      console.log(name);
+      app.addRoom(name);
+    }
+    for(var j = 0; j < app.messages[messages[i].roomname].length; j++){
       var itext = messages[i].text;
-      var jtext = app.messages[j].text;
-      if (itext === jtext) {
+      var jtext = app.messages[messages[i].roomname][j].text;
+      if(itext === jtext){
         hasDuplicate = true;
       }
     }
     if(!hasDuplicate){
-      app.messages.push(messages[i]);
+      app.messages[messages[i].roomname].push({message: messages[i], alreadyShown: false});
     }
   }
 }
 
+// var compareMessages = function(messages){
+//   //debugger;
+//   for(var i = 0; i < messages.length; i++) {
+//     var hasDuplicate = false;
+//     for(var j = 0; j < app.messages.length; j++){
+//       var itext = messages[i].text;
+//       var jtext = app.messages[j].text;
+//       if (itext === jtext) {
+//         hasDuplicate = true;
+//       }
+//     }
+//     if(!hasDuplicate){
+//       app.messages.push(messages[i]);
+//     }
+//   }
+// }
+
 app.addMessage = function(message){
-  var container = $('<p class="chat"></p>');
-  var chatTime = $('<span class="date"></span>').text("[" + message.createdAt + "] ");
-  var userName = $('<span class="username"></span>').text(message.username + ": ");
-  var chatText = $('<span class="messageText"></span>').text(message.text);
-  container.append(chatTime);
-  container.append(userName);
-  container.append(chatText);
-  $('#chats').prepend(container);
+  if (!message.alreadyShown){
+    var messageData = message.message;
+    var container = $('<p class="chat"></p>');
+    var chatTime = $('<span class="date"></span>').text("[" + messageData.createdAt + "] ");
+    var userName = $('<span class="username"></span>').text(messageData.username + ": ");
+    var chatText = $('<span class="messageText"></span>').text(messageData.text);
+    container.append(chatTime);
+    container.append(userName);
+    container.append(chatText);
+    $('#chats').prepend(container);
+    $('.username').on('click', function(){
+      console.log(this);
+      app.addFriend($(this).text().substring(0, $(this).text().length - 2)); //.attr('class')
+    })
+  }
 }
 
 app.addRoom = function(room){
-  $("#roomSelect").append('<span id="' + room + '">' + room + '</span>')
+  var newRoom = $('<button class="roomname">').attr('id', room).text(room);
+  $("#roomSelect").append(newRoom);
+  $("#" + room).on('click', function(){
+    app.currentRoom = $(this).attr('id');
+    app.clearMessages();
+  })
 }
 
-app.addFriend = function(){
-
+app.addFriend = function(friendName){
+  $('#friendlist').append($('<div>').attr('class', 'friend').text(friendName));
 }
 
 setInterval(function(){
@@ -81,15 +118,30 @@ $('document').ready(function(){
     app.send({
       'username': getName(),
       'text': $('.input').val(),
-      'roomname':'4chan'
+      'roomname': app.currentRoom,
     })
     $('.input').val("")
   }
 
   $('.addRoom').on('click', function(){
     app.addRoom($('.chatRoom').val())
+  })
 
+  $('.roomname').on('click', function(){
+    app.currentRoom = $(this).attr('id')
+    app.clearMessages()
   })
 
   $('.send').on('click', handleSubmit);
+
 })
+
+
+
+
+
+
+
+
+
+
